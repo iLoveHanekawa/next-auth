de511137cec7aff0f3c459f8993b6c7f74abfde4
Arjun Tanwar <arjunthakur900@gmail.com>
2023-04-14 14:50:57 +0530

    finished

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,14 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './auth/[...nextauth]'
 
 type Data = {
   name: string
+} | {
+  error: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  const session = await getServerSession(req, res, authOptions)
+
+  if(!session) {
+    res.status(401).json({ error: "Unauthorized" })
+  }
+
   console.log(process.env.GITHUB_SECRET)
   res.status(200).json({ name: 'John Doe' })
 }
